cabb08e069a78223e1881b1a1c139ace424076d5
Jan Kendrick Delos Lado <58259114+kendeloslado@users.noreply.github.com>
2024-04-17 17:19:57 +0800

    Add files via upload

--- QTableUpdate.v
+++ QTableUpdate.v
@@ -44,16 +44,76 @@ module QTableUpdate(clock, nrst, en, start, data_in, fSourceID, fEnergyLeft, fQV
         reg [4:0] state; 
         
         
+        /*
+        Some notes on reading and writing from memory
+        
+        You will use data_in to write into various registers in the program flow proper
+        
+        address_count will be used to navigate through memory, address_count will help 
+        output the correct data_in to respective registers
+        
+        Registers will be the following:
+        address_count -> assigned to output address
+        data_out_buf -> output data for stoof
+        neighborCount -> index for neighborID list
+        knownCHcount -> index for knownCHs list
+        cur_nID -> current node ID
+        cur_knownCH -> current known cluster head
+        cur_qValue -> current Q-value
+        done_buf -> buffer for the done signal
+        found ->  binary flag to indicate an entry is found in the table
+        reinit -> used for comparing some qValues and initial epsilon
+        (this is from CLIQUE, could be removed)
+        wr_en_buf ->  signal to enable writing to memory
+        n -> this is essentially a counter to navigate indexes for neighbor nodes
+        k -> counter to navigate knownCH indexes
+        */
+        
         // Program Flow Proper
         
         always@(posedge clock) begin
                 if(!nrst) begin
-                
+                        done_buf <= 0;
+                        address_count <= 0;
+                        state <= 0;
+                        wr_en_buf <= 0;
+                        data_out_buf <= 0;
+                        neighborCount <= 0;
+                        knownCHcount <= 0;
+                        cur_nID <= 0;
+                        cur_knownCH <= 0;
+                        cur_qValue <= 0;
+                        found <= 0;
+                        reinit <= 0;
+                        wr_en_buf <= 0;
                 end
                 else begin
-                
+                        case (state) 
+                                0: begin
+                                        state <= 1;
+                                        address_count <= 12'h274; //neighborCount address
+                                end
+                                1: begin
+                                        neighborCount <= data_in;
+                                        state <= 2;
+                                        address_count <= 12'h272; //knownCHcount address
+                                end
+                                2: begin
+                                        knownCHcount <= data_in;
+                                        state <= 3;
+                                end
+                                3: begin
+                                        // if not found, add a new neighbor in the entry
+                                        if (n == neighborCount)
+                                                state <= ; // go to  
+                                end
+                                default: state <= s_idle;
+                        endcase
                 end
         end
 
-
+        assign done = done_buf;
+        assign address = address_count;
+        assign data_out = data_out_buf;  
+        assign wr_en = wr_en_buf;
 endmodule
