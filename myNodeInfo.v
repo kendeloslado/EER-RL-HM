4291ed3e7dcbb5bbafe79a353088a27bb9eb25de
Jan Kendrick Delos Lado <jan.kendrick.delos.lado@eee.upd.edu.ph>
2024-08-22 14:01:50 +0800

    Update myNodeInfo.v

--- myNodeInfo.v
+++ myNodeInfo.v
@@ -12,6 +12,35 @@ module myNodeInfo(
     output                  low_E
 );
 
+    localparam MY_NODE_ID_CONST = 16'h000C; // example node ID
+
+    // Registers
+    reg     [15:0]          myNodeID_reg;
+    reg     [15:0]          hopsFromSink_reg;
+    reg     [15:0]          myQValue_reg;
+    reg     [15:0]          e_threshold_reg;
+    reg     [15:0]          timeslot;
+    reg                     HBLock_reg;
+    reg                     role_reg;
+
+    // always block for role
+
+    always@(posedge clk) begin
+        if(!nrst) begin
+            role <= 0;
+        end
+        else begin
+            if(en_MNI) begin
+                if(nodeID == ch_ID)
+                    role <= 1;
+                else
+                    role <= 0;
+            end
+            else begin
+                role <= role;
+            end
+        end
+    end
     
 
 endmodule
