3a7fed3b82af2bd2b3ccfb3f44a0c48c31869b9f
Jan Kendrick Delos Lado <jan.kendrick.delos.lado@eee.upd.edu.ph>
2024-08-22 18:51:14 +0800

    Update myNodeInfo.v

--- myNodeInfo.v
+++ myNodeInfo.v
@@ -2,6 +2,7 @@ module myNodeInfo(
     input                   clk,
     input                   nrst,
     input                   en_MNI,
+    input   [2:0]           fPktType,
     input   [15:0]          e_max,
     input   [15:0]          e_min,
     input   [15:0]          energy,
@@ -18,28 +19,28 @@ module myNodeInfo(
     localparam MY_NODE_ID_CONST = 16'h000C; // example node ID
 
     // Registers
-    reg     [15:0]          hopsFromSink_reg;
-    reg     [15:0]          myQValue_reg;
-    reg     [15:0]          e_threshold_reg;
+    reg     [15:0]          hopsFromSink_buf;
+    reg     [15:0]          myQValue_buf;
+    reg     [15:0]          e_threshold_buf;
     reg     [15:0]          timeslot;
-    reg                     HBLock_reg;
-    reg                     role_reg;
-    reg                     low_E_reg;
+    reg                     HBLock_buf;
+    reg                     role_buf;
+    reg                     low_E_buf;
 
 // always block for role
     always@(posedge clk) begin
         if(!nrst) begin
-            role <= 0;
+            role_buf <= 0;
         end
         else begin
-            if(en_MNI) begin
+            if(en_MNI && fPktType == 3'b001) begin
                 if(nodeID == ch_ID)
-                    role <= 1;
+                    role_buf <= 1;
                 else
-                    role <= 0;
+                    role_buf <= 0;
             end
             else begin
-                role <= role;
+                role_buf <= role_buf;
             end
         end
     end
@@ -47,30 +48,41 @@ module myNodeInfo(
 // always block for low_E
     always@(posedge clk) begin
         if(!nrst) begin
-            low_E <= 0;
+            low_E_buf <= 0;
         end
         else begin
-            if(en_MNI) begin
-                if(energy < e_threshold) begin
-                    low_E <= 1;
-                end
-                else begin
-                    low_E <= 0;
-                end
+            if(energy < e_threshold) begin
+                low_E_buf <= 1;
             end
             else begin
-                low_E <= low_E;
+                low_E_buf <= 0;
             end
         end
     end
 // always block for hopsFromSink_buf
+    always@(posedge clk) begin
+        if(!nrst) begin
+            hopsFromSink_buf <= 0;
+        end
+        else begin
+            if(en_MNI && fPktType == 3'b000) begin
+            // leaving the enable condition to just en_MNI presents some problems.
+            // get an additional condition in. change it later.
+            // i.e. if(fPktType == 3'b000;)
+                hopsFromSink_buf <= hops;
+            end  
+            else begin
+                hopsFromSink_buf <= hopsFromSink_buf;
+            end
+        end
+    end
 
 
 // assign outputs to register buffers
 assign myNodeID = MY_NODE_ID_CONST;
-assign hopsFromSink = hopsFromSink_reg;
-assign myQValue = myQValue_reg;
-assign role = role_reg;
-assign low_E = low_E_reg;
+assign hopsFromSink = hopsFromSink_buf;
+assign myQValue = myQValue_buf;
+assign role = role_buf;
+assign low_E = low_E_buf;
 
 endmodule
