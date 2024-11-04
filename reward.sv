ee41c049966d21fa2f3bb71f72718b76fca03bd4
Jan Kendrick Delos Lado <jan.kendrick.delos.lado@eee.upd.edu.ph>
2024-11-04 21:31:15 +0800

    Update reward.sv

--- reward.sv
+++ reward.sv
@@ -5,7 +5,45 @@ module reward #(
     parameter MEM_WIDTH = 8,
     parameter WORD_WIDTH = 16
 )(
-    
+// global inputs
+    input logic                         clk,
+    input logic                         nrst,
+// MY_NODE_INFO inputs
+    input logic     [WORD_WIDTH-1:0]    myNodeID,
+    input logic     [WORD_WIDTH-1:0]    hopsFromSink,
+    input logic     [WORD_WIDTH-1:0]    myQValue,
+    input logic     [WORD_WIDTH-1:0]    myEnergy,
+// kCH inputs
+    input logic     [WORD_WIDTH-1:0]    chosenCH,
+    input logic     [WORD_WIDTH-1:0]    hopsFromCH, 
+// reward outputs
+    output logic    [WORD_WIDTH-1:0]    rSourceID,
+    output logic    [WORD_WIDTH-1:0]    rEnergyLeft,
+    output logic    [WORD_WIDTH-1:0]    rQValue,
+    output logic    [WORD_WIDTH-1:0]    rSourceHops,
+    output logic    [WORD_WIDTH-1:0]    rDestinationID,
+    output logic    [WORD_WIDTH-1:0]    rPacketType,
+    output logic    [WORD_WIDTH-1:0]    rChosenCH,
+    output logic    [WORD_WIDTH-1:0]    rHopsFromCH,
+// output signal
+    output logic    [WORD_WIDTH-1:0]    reward_done
 );
+
+// reward block essentials
+
+/* 
+    The reward block is in charge of packing data into a data packet to be sent
+    to the next hop. The reward block will forward the packet to another node, 
+    a cluster head, or the sink. The type of packet the reward will pack depends
+    on the incoming packet the node has received.
+
+    Reward block needs to pack data when the condition is one of the ff.:
+    1. The node has received a Heartbeat Packet (HB);
+    2. The node has received an Invitation Packet (INV), whose hopsFromCH count 
+    is less than 4;
+    3. The node needs to send a Membership Request packet
+    4. The node received a data packet whose destinationID is the node itself.
+    5. 
+ */
 
 endmodule
