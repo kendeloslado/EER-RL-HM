ad573f35b9c3fb63bdc18ce8cf30eb012f8c4ec1
Jan Kendrick Delos Lado <jan.kendrick.delos.lado@eee.upd.edu.ph>
2024-11-06 12:23:57 +0800

    Update reward.sv

--- reward.sv
+++ reward.sv
@@ -9,6 +9,8 @@ module reward #(
     input logic                         clk,
     input logic                         nrst,
     input logic                         en,
+// signal from packetFilter
+    input logic                         iAmDestination,
 // MY_NODE_INFO inputs
     input logic     [WORD_WIDTH-1:0]    myNodeID,
     input logic     [WORD_WIDTH-1:0]    hopsFromSink,
@@ -46,7 +48,9 @@ module reward #(
     on the incoming packet the node has received.
 
     Reward block needs to pack data when the condition is one of the ff.:
+
     1. The node has received a Heartbeat Packet (HB);
+
         To determine whether you received a heartbeat packet, you can use the 
     fPacketType from a previous module (packetFilter). That's one of the signals
     down, but you need another signal in order to prevent sending duplicate HB
@@ -55,31 +59,42 @@ module reward #(
 
     2. The node has received an Invitation Packet (INV), whose hopsFromCH count 
     is less than 4. If true, the node ripples the invitation packet;
+
         Before the node ripples the invitation packet, the node should check the
     hopsFromCH field to see if it's less than 4. If this is true, before packing
     the data, the node needs to increment 1 to the hopsFromCH before rippling it.
 
-    3. The node needs to send a Membership Request packet, triggered by a timeout signal.
+    3. The node sends a Membership Request packet, triggered by a timeout signal.
+
         There's a register that is set at a certain count during Cluster Formation. It 
     will decrement by 1 until it reaches 0. When it reaches 0, this is the time for the
     individual node to start sending a membership request packet to their desired
     cluster head.
 
-    4. The node received a data/SOS packet whose destinationID is the node itself,
-    and the node needs to send their data to their nexthop.
-        Trigger condition is simple, the node must be in communication phase, and 
-    it receives a packet whose destinationID is directed to them.
+    4. The node receives a data/SOS packet whose destinationID is the node itself,
+    and the node needs to send their data to their nexthop;
+
+        Trigger condition is that the node must receive a packet whose destinationID is 
+    directed to them.
     
-    5. The node is a cluster head and they need to pack invitation packets.
+    5. The node is a cluster head and they need to pack invitation packets;
+
         The sink will assign cluster heads using a cluster head election packet.
     Once a node has been elected cluster head, the node begins packing their info
     to the packet and broadcasts them to the network. The reward block will be
     turned on as a result of packing their information into the INV pkt.
 
     6. The node is a cluster head and they need to send CH Timeslots.
-        The node will wait on a timeout register while receiving membership request
-    packets from neighboring nodes. 
+
+        The node will wait on a timeout register while waiting for membership 
+    request packets from neighboring nodes. When this timeout register runs out,
+    the cluster head will begin sending CH timeslot packets to its cluster members.
     
+    7. The node has received enough information and needs to send data to their nexthop.
+
+        This particular condition is not exactly defined, but a certain signal needs to
+    be asserted if the node wants to send data. The data sending proper is not covered
+    in this block, as the reward block packs data relating to node information.
 
  */
 
@@ -89,9 +104,44 @@ module reward #(
     s_done = finish.
  */
 
+ /* 
+ Ma'am Belay questions:
+ Related sa network. Prior to communication phase, ang protocol nya is CSMA, after cluster formation, TDMA na yung protocol.
+ As of now, it is assumed there's a separate block handling CSMA. Check current implementations what the assumed packet would look
+ like. Tignan mo kung pwede gamitin. anong information dinadala mo everytime? Identify ilang chunks yung isesend mo, etc.
+
+ Ang importante, clear dapat yung flow of information
+
+ week of november 10 discussion
+ Assuming mabuo yung system, ano yung testing methodology mo? Identify the scenarios (i.e. cluster member, cluster heda node, node is one hop from the sink)
+
+Wednesday after Lunch should be okay (mga around 2pm onward) [November 13]
+
+  */
+
 // internal registers for the module
-    logic       [1:0]           state;
-    
+    logic       [1:0]               state;
+    logic       [3:0]               FBType; // type of packet to pack in the reward block
+    logic       [WORD_WIDTH-1:0]    timeout; // maybe one timeout will be used. Current timeout value is 10.
+    logic                           timeout_type; // INV timeout or MR timeout.
+
+/* 
+    FBType descriptions:
+    3'b0000:    Node has received a Heartbeat (HB) packet and is required to ripple. 
+                Trigger condition: packetType == 3'b000 && HBLock == 0;
+    3'b0001:    Node has received an INV pkt. 
+                Trigger condition: packetType == 3'b010 && hopsFromCH < 4;
+    3'b0010:    Node is sending a membership request packet. 
+                Trigger condition: timeout == 0 && timeout_type == 0;
+    3'b0011:    Node is sending a Data/SOS packet. 
+                Trigger condition: iAmDestination.
+    3'b0100:    Node is a CH and should send INV pkts. 
+                Trigger condition: role == 1;
+    3'b0101:    Node is a CH and should send CH Timeslot pkts. 
+                Trigger condition: mr_timeout == 0 && timeout_type == 1;
+    3'b0110:    Node is the source and should send data packet. 
+                Trigger condition: [some_sender_defined_signal] == 1;
+ */
 
 
 endmodule
