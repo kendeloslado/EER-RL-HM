948bd18cc08faa4957d539bcdb5fa5ccbbd1748f
Jan Kendrick Delos Lado <jan.kendrick.delos.lado@eee.upd.edu.ph>
2025-01-21 18:43:31 +0800

    Update tb_myNodeInfo.v
    update for CH Timeslot simulations

--- tb_myNodeInfo.v
+++ tb_myNodeInfo.v
@@ -6,6 +6,7 @@
 `define RX_PKT_NRG      16'h0004
 `define HOP1_TX         16'h0005
 `define HOP4_TX         16'h001b
+`define CLOCK_CYCLE     20
 module tb_myNodeInfo();
 
     reg                   clk;
@@ -55,10 +56,10 @@ initial begin
     timeslot = 16'hffff;
 // Let's simulate receiving a heartbeat packet first
     
-    #100
+    #(`CLOCK_CYCLE * 5)
 // starting up myNodeInfo
     nrst = 1;
-    #40 // let it settle
+    #(`CLOCK_CYCLE * 2) // let it settle
 // receive heartbeat packet
     fPktType = 3'b000;
     hops = 1;
@@ -67,11 +68,11 @@ initial begin
     energy = energy - `RX_PKT_NRG;
     e_threshold = 16'h3333; // 14./2 fixed-point == 0.8
     // no timeslot, destinationID
-    #40
+    #(`CLOCK_CYCLE * 2)
     en_MNI = 1;
-    #20
+    #`CLOCK_CYCLE
     en_MNI = 0;
-    #40
+    #(`CLOCK_CYCLE * 2)
 // try to receive another heartbeat packet
     // in this scenario, the node should drop the HB packet
     fPktType = 3'b000;
@@ -80,26 +81,26 @@ initial begin
     e_min = 16'h4000; */
     energy = energy - `RX_PKT_NRG;  // fixed-point ~= 1.9961
     e_threshold = 16'h3333;
-    #40
+    #(`CLOCK_CYCLE *2)
     en_MNI = 1;
-    #20
+    #`CLOCK_CYCLE
     en_MNI = 0;
-    #40
+    #(`CLOCK_CYCLE *2)
 // see if anything changes.
     
     // let it cook
     // After enabling myNodeInfo, the individual should have recorded the information 
     // attached above, outputting an initial Q-value, and hopsFromSink
-    #40
+    #(`CLOCK_CYCLE *2)
 // receive CHE packet
     fPktType = 3'b001;
     destinationID = 16'd32; // sample destinationID, not cluster head for this CH.
     energy = energy - `RX_PKT_NRG;
-    #20
+    #`CLOCK_CYCLE
     en_MNI = 1; // go check if you're a CH for the round
-    #20
+    #`CLOCK_CYCLE
     en_MNI = 0;
-    #40
+    #(`CLOCK_CYCLE *2)
     // by the time you get to this delay, role should still be at 0.
 /* // receive an INV packet
     fPktType = 3'b010; // this packet type should not do anything in the node
@@ -110,7 +111,7 @@ initial begin
     #20
     en_MNI = 0;
     #40 */
-// receive another CHE packet
+/* // receive another CHE packet
     fPktType = 3'b001;
     destinationID = 16'h000C; // sample nodeID constant is set at 16'h000C. This
                       // packet should change your role into 1.
@@ -119,21 +120,21 @@ initial begin
     en_MNI = 1;
     #20
     en_MNI = 0;
-    #100
+    #100 */
      // let it update
-/* // receive a CHTimeslot packet
+// receive a CHTimeslot packet
     // this CHTimeslot packet should not do anything since you're a CH in
     // this testbench
     fPktType = 3'b100;
-    timeslot = 3'd4;        // simulate that you are at timeslot #4
-    destinationID = 8'd21;  // not your ID
+    timeslot = 16'd1;        // simulate that you are at timeslot #1
+    destinationID = 16'd12;  // this is your ID
     hops = 2;
     energy = energy - `RX_PKT_NRG;
-    #20
+    #`CLOCK_CYCLE
     en_MNI = 1;
-    #20
+    #`CLOCK_CYCLE
     en_MNI = 0;          
-    #100 */
+    #(`CLOCK_CYCLE *5)
 /* // receive a data packet
     // this data packet should de-assert HBLock
     fPktType = 3'b101;
